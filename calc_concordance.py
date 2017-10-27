52cabc298f308e4653ff37aa6fa36a0252836511
Jeff Wintersinger <jeff.git@wintersinger.com>
2017-10-27 14:29:19 -0400

    Print each pair's concordance rank.

--- calc_concordance.py
+++ calc_concordance.py
@@ -3,10 +3,13 @@ from pwgsresults.result_loader import ResultLoader as PwgsResults
 import re
 import argparse
 import numpy as np
+import itertools
 
-def euclid(A, B):
+def lpdist(A, B, p=2):
   A, B = np.array(A), np.array(B)
-  return np.sqrt(np.sum((A - B)**2))
+  dist = np.sum(np.abs(A - B)**p)**(1./p)
+  assert dist >= 0
+  return dist
 
 def compare(s1idx, s2idx, tidx, treesumm):
   pops = treesumm[tidx]['populations']
@@ -16,11 +19,21 @@ def compare(s1idx, s2idx, tidx, treesumm):
   for pop in pops.values():
     s1cp.append(pop['cellular_prevalence'][s1idx])
     s2cp.append(pop['cellular_prevalence'][s2idx])
-  return euclid(s1cp, s2cp)
+  return lpdist(s1cp, s2cp, p=1)
 
 def calc_concordance(samples, treesumm):
   #Use handbuilt tree.
   tidx = 0
+
+  allsampidxs = [idx for idx, S in enumerate(samples) if re.search(r'^Diagnosis Xeno', S)]
+  distlist = []
+  for A, B in itertools.combinations(allsampidxs, 2):
+    dist = compare(A, B, tidx, treesumm)
+    distlist.append(( (A, B), dist  ))
+  distlist.sort(key = lambda E: E[-1])
+  dists = {}
+  for pos, ((A, B), dist) in enumerate(distlist):
+    dists[(A, B)] = dists[(B, A)] = (dist, (pos + 1.) / len(distlist))
 
   for sampidx, sampname in enumerate(samples):
     if not re.search(r'^Diagnosis Xeno \d+$', sampname):
@@ -29,12 +42,20 @@ def calc_concordance(samples, treesumm):
     for suffix in ('CNS', 'Spleen'):
       other = '%s %s' % (sampname, suffix)
       if other not in samples:
+        print(sampname, other, 'absent', sep='\t')
         continue
       otheridx = samples.index(other)
-      dist = compare(sampidx, otheridx, tidx, treesumm)
       # Since every CP is in [0, 1], we can get normalized distance by dividing
       # by numer of samples.
-      print(sampname, other, dist, dist / len(samples), sep='\t')
+      dist, rank = dists[(sampidx, otheridx)]
+      print(
+        sampname,
+        other,
+        dist,
+        dist / len(samples),
+        rank,
+        sep='\t'
+      )
 
 def main():
   parser = argparse.ArgumentParser(
