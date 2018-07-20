e609ab5d601a678383b8bca0201fed3d8b373c15
Jeff Wintersinger <jeff.git@wintersinger.com>
2018-07-20 13:55:48 -0400

    Include JS and CSS inline to simplify deployment.

--- pairtree.py
+++ pairtree.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import numpy as np
 
 import common
@@ -45,16 +46,21 @@ def fit_phis(adjm, variants, clusters, tidxs=None, parallel=1):
 
   return (phi, eta)
 
+def read_file(fn):
+  basedir = os.path.abspath(os.path.dirname(__file__))
+  with open(os.path.join(basedir, fn)) as F:
+    return F.read()
+
 def write_header(sampid, outf):
   print('<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>', file=outf)
   print('<script src="https://d3js.org/d3.v4.min.js"></script>', file=outf)
   print('<script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>', file=outf)
-  print('<script type="text/javascript" src="highlight_table_labels.js"></script>', file=outf)
-  print('<script type="text/javascript" src="tree_plotter.js"></script>', file=outf)
+  print('<script type="text/javascript">%s</script>' % read_file('highlight_table_labels.js'), file=outf)
+  print('<script type="text/javascript">%s</script>' % read_file('tree_plotter.js'), file=outf)
   print('<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">', file=outf)
   print('<h1>%s</h1>' % sampid, file=outf)
-  print('<link href="tree.css" rel="stylesheet">', file=outf)
-  print('<style>td, th, table { padding: 5px; margin: 0; border-collapse: collapse; font-weight: normal; } span { visibility: hidden; } td:hover > span { visibility: visible; } .highlighted { background-color: black !important; color: white; }</style>', file=outf)
+  print('<style type="text/css">%s</style>' % read_file('tree.css'), file=outf)
+  print('<style type="text/css">td, th, table { padding: 5px; margin: 0; border-collapse: collapse; font-weight: normal; } span { visibility: hidden; } td:hover > span { visibility: visible; } .highlighted { background-color: black !important; color: white; }</style>', file=outf)
 
 def write_trees(sampid, tidx, outf):
   colourings = {
