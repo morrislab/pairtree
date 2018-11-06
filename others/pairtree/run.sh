e3aab50114f9b0d82da4dfdba895ae1007907b4e
Jeff Wintersinger <jeff.git@wintersinger.com>
2018-11-05 20:58:50 -0500

    Create mutrel results for Pairtree.

--- others/pairtree/run.sh
+++ others/pairtree/run.sh
@@ -2,16 +2,15 @@
 set -euo pipefail
 module load gnu-parallel
 
+SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 BASEDIR=~/work/pairtree
 JOBDIR=$SCRATCH/jobs
-INDIR=$BASEDIR/scratch/inputs/steph.xeno.nogarb.pwgs
-OUTBASE=$BASEDIR/scratch/results/steph.xeno.nogarb.pwgs
-PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/steph.xeno.nogarb.pairtree
 HANDBUILTDIR=~/work/steph/data/handbuilt_trees
+PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/steph.xeno.nogarb.pairtree
 PHYLOSTEPH_INPUTS_DIR=~/work/steph/data/inputs/steph.xenos.nocns
+PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/steph.xeno.nogarb.pairtree
 TRUTH_DIR=$BASEDIR/scratch/results/steph.xeno.nogarb.truth
 PARALLEL=40
-SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 
 function convert_phylosteph_inputs {
   mkdir -p $PAIRTREE_INPUTS_DIR
@@ -36,9 +35,21 @@ function make_truth_mutrels {
   done | parallel -j$PARALLEL --halt 1
 }
 
+function create_mutrels {
+  for resultsfn in $PAIRTREE_RESULTS_DIR/*.results.npz; do
+    runid=$(basename $resultsfn | cut -d. -f1)
+    echo "cd $PAIRTREE_RESULTS_DIR &&" \
+      "python3 $SCRIPTDIR/convert_outputs.py" \
+      "$PAIRTREE_INPUTS_DIR/$runid.params.json" \
+      "$PAIRTREE_RESULTS_DIR/$runid.results.npz" \
+      "$PAIRTREE_RESULTS_DIR/$runid.{pairs,trees}.mutrel.npz"
+  done | parallel -j$PARALLEL --halt 1
+}
+
 function main {
   #convert_phylosteph_inputs
-  make_truth_mutrels
+  #make_truth_mutrels
+  create_mutrels
 }
 
 main
