61f2ffd4ea87ce71e280e9e43b73accdc143ce28
Jeff Wintersinger <jeff.git@wintersinger.com>
2018-11-08 08:56:05 -0500

    Print scores in appropriate directory.

--- others/run_eval.sh
+++ others/run_eval.sh
@@ -9,21 +9,25 @@ RESULTSDIR=$BASEDIR/scratch/results
 function main {
   cd $RESULTSDIR
   prefix=steph.xeno.nogarb
+  SCORESDIR=$RESULTSDIR/$prefix/scores
+  mkdir -p $SCORESDIR
 
   for mutrelfn in ${prefix}.truth/*.mutrel.npz; do
     runid=$(basename $mutrelfn | cut -d. -f1)
     mutrels="truth=${prefix}.truth/$runid.mutrel.npz "
     mutrels+="pwgs=${prefix}.pwgs/$runid/$runid.mutrel.npz "
-    mutrels+="pairtree_trees=${prefix}.pairtree/$runid.trees.mutrel.npz"
+    mutrels+="pairtree_trees=${prefix}.pairtree/$runid.trees.mutrel.npz "
+    mutrels+="pairtree_pairs=${prefix}.pairtree/$runid.pairs.mutrel.npz"
 
     for M in $(echo $mutrels | tr ' ' '\n' | cut -d= -f2); do
       [[ ! -f $M ]] && continue 2
     done
 
-    echo "cd $RESULTSDIR && python3 $SCRIPTDIR/eval.py $mutrels > $runid.score.txt"
+    echo "cd $RESULTSDIR && python3 $SCRIPTDIR/eval.py $mutrels > $SCORESDIR/$runid.score.txt"
   done | parallel -j40 --halt 0
 
   (
+    cd $SCORESDIR
     echo 'runid,'$(head -n1 $(ls *.score.txt | head -n1))
     for foo in *.score.txt; do
       echo $(echo $foo | cut -d. -f1),$(tail -n+2 $foo) 
