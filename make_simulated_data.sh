7d0b1465fc0df4cfbff1581f513baa54beb523f1
Jeff Wintersinger <jeff.git@wintersinger.com>
2018-12-30 17:53:06 -0500

    Change range of simulation parameters.

--- make_simulated_data.sh
+++ make_simulated_data.sh
@@ -14,21 +14,17 @@ function make_simulated_data {
   mkdir -p $INDIR
   module load gnu-parallel
 
-  for K in 2 4 8; do
-  for S in 1 3 10 30; do
+  for K in 3 10; do
+  for S in 1 3 10 30 100; do
   for T in 50 200 1000; do
-  for M in 20 50 100; do
+  for M_per_cluster in 10 20 100; do
+  for G_frac in 0 0.01 0.1 1; do
   for run in $(seq 1); do
-    G=$(expr $M / $FRACGARB) # Takes floor of dividend
+    M=$(echo "$K * $M_per_cluster" | bc)
+    G=$(echo "(($G_frac * $M) + 0.5) / 1" | bc)
+
     jobname="sim_K${K}_S${S}_T${T}_M${M}_G${G}_run${run}"
     (
-      #echo "#!/bin/bash"
-      #echo "#SBATCH --nodes=1"
-      #echo "#SBATCH --ntasks=$PARALLEL"
-      #echo "#SBATCH --time=24:00:00"
-      #echo "#SBATCH --job-name $jobname"
-      #echo "#SBATCH --output=$JOBDIR/${jobname}.%j.log"
-      #echo "#SBATCH --mail-type=FAIL"
       echo "python3 $PROTDIR/make_simulated_data.py" \
         "--write-clusters" \
         "-K $K" \
@@ -42,6 +38,7 @@ function make_simulated_data {
         "> $INDIR/$jobname.stdout" \
         "2>$INDIR/$jobname.stderr" \
     )
+  done
   done
   done
   done
