8caff76adcf436e86bd67d609b8bd80aeecf00b5
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-01-08 19:07:36 -0500

    Run output conversion through SLURM.

--- comparison/pairtree/run.sh
+++ comparison/pairtree/run.sh
@@ -9,7 +9,7 @@ JOBDIR=~/jobs
 PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/sims.pairtree
 PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/sims.pairtree.fixedclusters
 
-PARALLEL=80
+PARALLEL=40
 TREE_CHAINS=40
 BURNIN_PER_CHAIN=1000
 TREES_PER_CHAIN=2000
@@ -18,7 +18,8 @@ PHI_ITERATIONS=10000
 function run_pairtree {
   mkdir -p $PAIRTREE_RESULTS_DIR
 
-  for ssmfn in $(ls $PAIRTREE_INPUTS_DIR/*.ssm | sort --random-sort | head -n50); do
+  #for ssmfn in $(ls $PAIRTREE_INPUTS_DIR/*.ssm | sort --random-sort | head -n50); do
+  for ssmfn in $PAIRTREE_INPUTS_DIR/*.ssm; do
     runid=$(basename $ssmfn | cut -d. -f1)
     jobfn=$(mktemp)
     (
@@ -52,18 +53,34 @@ function run_pairtree {
 function convert_outputs {
   for resultsfn in $PAIRTREE_RESULTS_DIR/*.results.npz; do
     runid=$(basename $resultsfn | cut -d. -f1)
-    echo "cd $PAIRTREE_RESULTS_DIR &&" \
-      "python3 $SCRIPTDIR/convert_outputs.py" \
-      "--clustrel-mutrel $PAIRTREE_RESULTS_DIR/$runid.pairtree_clustrel.mutrel.npz" \
-      "--trees-mutrel $PAIRTREE_RESULTS_DIR/$runid.pairtree_trees.mutrel.npz" \
-      "--phi $PAIRTREE_RESULTS_DIR/$runid.pairtree.phi.npz" \
-      "$PAIRTREE_RESULTS_DIR/$runid.results.npz"
-  done | parallel -j$PARALLEL --halt 1
+    jobfn=$(mktemp)
+    (
+        echo "#!/bin/bash"
+        echo "#SBATCH --nodes=1"
+        echo "#SBATCH --ntasks=$PARALLEL"
+        echo "#SBATCH --time=23:59:00"
+        echo "#SBATCH --job-name convert_$runid"
+        echo "#SBATCH --output=$JOBDIR/slurm_${runid}_%j.txt"
+        echo "#SBATCH --mail-type=NONE"
+
+        echo "cd $PAIRTREE_RESULTS_DIR &&" \
+          "python3 $SCRIPTDIR/convert_outputs.py" \
+          "--weight-trees-by uniform" \
+          "--clustrel-mutrel $PAIRTREE_RESULTS_DIR/$runid.pairtree_clustrel.mutrel.npz" \
+          "--trees-mutrel $PAIRTREE_RESULTS_DIR/$runid.pairtree_trees.mutrel.npz" \
+          "--phi $PAIRTREE_RESULTS_DIR/$runid.pairtree.phi.npz" \
+          "$PAIRTREE_RESULTS_DIR/$runid.results.npz" \
+          ">$PAIRTREE_RESULTS_DIR/$runid.output_conversion.stdout" \
+          "2>$PAIRTREE_RESULTS_DIR/$runid.output_conversion.stderr"
+    ) > $jobfn
+    sbatch $jobfn
+    rm $jobfn
+  done
 }
 
 function main {
-  run_pairtree
-  #convert_outputs
+  #run_pairtree
+  convert_outputs
 }
 
 main
