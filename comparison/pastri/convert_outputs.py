680ba4563c8f394d6e57c3ad4fcd2c8ebae8f8f5
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-01-19 11:59:37 -0500

    Generate mutrels from PASTRI.

--- comparison/pastri/convert_outputs.py
+++ comparison/pastri/convert_outputs.py
@@ -2,6 +2,13 @@ import argparse
 from io import StringIO
 import numpy as np
 
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+import inputparser
+import evalutil
+
 def load_matrices(matfn, dtype=np.float):
   mats = {}
   matname = None
@@ -17,6 +24,8 @@ def load_matrices(matfn, dtype=np.float):
         mats[matname] = ''
       elif line.startswith('(') and line.endswith(')'):
         # This is just the shape of the matrix. Ignore.
+        # Note that "tree labelling" files don't have this header. We don't
+        # require it as a consequence.
         pass
       else:
         mats[matname] += line + '\n'
@@ -25,68 +34,137 @@ def load_matrices(matfn, dtype=np.float):
     mats[matname] = np.loadtxt(StringIO(mats[matname]), dtype=dtype)
   return mats
 
-def find_parents_from_ancestral(anc):
-  # Basic idea: determine how many ancestors each node `i` has by examining the
-  # nonzero entries in column `i`. If I do this in order of number of
-  # ancestors, the parent of `i` is guaranteed to have already been placed in
-  # the tree. When looking at all possible parents of `i` from amongst `i`'s
-  # ancestors, choose the parent that has the greatest depth in the tree, as
-  # this must be the direct parent.
-  anc = np.copy(anc)
-  np.fill_diagonal(anc, 0)
-  K = len(anc)
-  num_anc = np.sum(anc, axis=0)
-
-  parents = np.nan * np.ones(K)
-  depth = {}
-
-  for I in np.flatnonzero(num_anc == 0):
-    parents[I] = -1
-    depth[I] = 0
-  assert np.sum(parents == -1) == 1
-
-  for A in range(1, K):
-    if not np.any(np.isnan(parents)):
-      break
-    for I in np.flatnonzero(num_anc == A):
-      possible_parents = np.flatnonzero(anc[:,I])
-      assert len(possible_parents) > 0
-      pp_depth = [(P, depth[P]) for P in possible_parents]
-      # Sort by maximum depth.
-      pp_depth.sort(key = lambda pair: -pair[1])
-      # Ensure all possible parents are at unique depths.
-      assert len(set([D for P, D in pp_depth])) == len(possible_parents)
-      parent = pp_depth[0][0]
-      parents[I] = parent
-      depth[I] = A
-
-  assert not np.any(np.isnan(parents))
-  return parents
-    
-def load_trees(trees_fn):
+def load_prelim_trees(trees_fn):
+  # These are initial trees, representing trees with distinct LHs. Structure is
+  # an ancestral matrix, not an adjacency matrix. Multiple permutations of the
+  # nodes may be possible (all with the same LH, as their frequencies stay the
+  # same), but those aren't listed here. We load the trees here to figure out
+  # which sampled frequencies have a non-zero LH.
   mats = load_matrices(trees_fn, dtype=np.int)
   trees = {}
+
   for K in mats:
     fields = K.split(':')
     assert len(fields) == 3
+
     tree_id = int(fields[1])
-    assert tree_id not in trees
-    trees[tree_id] = {
+    tree = {
       'rank': int(fields[0]),
       'llh': float(fields[2]),
-      'structure': find_parents_from_ancestral(mats[K]),
+      'structure': mats[K],
     }
+    if tree['llh'] > -np.inf:
+      assert tree_id not in trees
+      trees[tree_id] = tree
+
   return trees
+
+def load_final_trees(trees_fn):
+  # These are final trees, representing all possible permutations of nodes with
+  # the sampled frequencies that are consistent with any tree structure. Tree
+  # structure is provided as a Kx2 matrix of edges.
+  mats = load_matrices(trees_fn, dtype=np.int)
+  adjms = []
+
+  for edges in mats.values():
+    parents, children = edges[:,0], edges[:,1]
+    max_child = np.max(children)
+    assert 0 in parents and 0 not in children
+
+    adjm = np.eye(max_child + 1)
+    adjm[parents,children] = 1
+    adjms.append(adjm)
+
+  return adjms
+
+def load_clusters(clustfn, K):
+  # K: number of clusters
+  # Clusters aren't guaranteed to be contiguous -- even if you have `K` nodes
+  # in the tree, some of those can have zero mutations assigned. Hence, I force
+  # a list at every possible cluster index.
+  clusters = {cidx: [] for cidx in range(K)}
+
+  with open(clustfn) as F:
+    for line in F:
+      tokens = [int(T) for T in line.strip().split()]
+      parent, children = tokens[0], tokens[1:]
+      clusters[parent] += children
+
+  clusters = [clusters[C] for C in sorted(clusters.keys())]
+  return clusters
+
+def convert_pastri_clusters(pastri_clusters, variant_clusters):
+  converted = []
+  for pcluster in pastri_clusters:
+    converted.append([])
+    for vcidx in pcluster:
+      converted[-1] += variant_clusters[vcidx]
+  return converted
+
+def convert_results(sampid, prelim_trees, variant_clusters, outdir):
+  _makefn = lambda tidx, ext: os.path.join(outdir, '%s.%s.%s' % (sampid, tidx, ext))
+  llhs = []
+  adjms = []
+  clusterings = []
+
+  for tree in prelim_trees.values():
+    tidx = tree['rank']
+
+    freqfn = _makefn(tidx, 'F')
+    # Sometimes, if PASTRI's get_F_and_C.py crashed, this file won't exist,
+    # even though the corresponding tree in the .trees file will have non-zero
+    # LH.
+    if not os.path.exists(freqfn):
+      continue
+    freqs = load_matrices(freqfn, np.float)
+    assert len(freqs) == 1
+    F = next(iter(freqs.values()))
+
+    K = len(F)
+    clustfn = _makefn(tidx, 'C')
+    C = load_clusters(clustfn, K)
+    clusters = convert_pastri_clusters(C, variant_clusters)
+
+    treefn = _makefn(tidx, 'labeled_trees')
+    tree_adjms = load_final_trees(treefn)
+    for A in tree_adjms:
+      adjms.append(A)
+      # There are multiple possible permutations of tree nodes within this
+      # structure. All have the same LLH and clustering.
+      llhs.append(tree['llh'])
+      clusterings.append(clusters)
+
+  assert len(adjms) == len(llhs) == len(clusterings)
+  return (adjms, llhs, clusterings)
+
+def write_mutrel(sampid, params_fn, trees_fn, tree_weights, trees_mutrel_fn):
+  params = inputparser.load_params(params_fn)
+  outdir = os.path.dirname(trees_fn)
+  prelim_trees = load_prelim_trees(trees_fn)
+
+  adjms, llhs, clusterings = convert_results(sampid, prelim_trees, params['clusters'], outdir)
+  if len(adjms) == 0:
+    return
+  mrel = evalutil.calc_mutrel_from_trees(adjms, llhs, clusterings, tree_weights)
+  evalutil.save_sorted_mutrel(mrel, trees_mutrel_fn)
 
 def main():
   parser = argparse.ArgumentParser(
     description='LOL HI THERE',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
   )
+  parser.add_argument('--weight-trees-by', choices=('llh', 'uniform'), default='uniform')
+  # This takes Pairtree rather than PWGS inputs, which seems a little weird,
+  # but it's okay -- the PWGS inputs are the supervariants, ut we need to know
+  # hich variants correspond to each cluster in the original Pairtree inputs.
+  parser.add_argument('--trees-mutrel')
+  parser.add_argument('sampid')
+  parser.add_argument('pairtree_params_fn')
   parser.add_argument('trees_fn')
   args = parser.parse_args()
-  trees = load_trees(args.trees_fn)
-  print(trees)
+
+  if args.trees_mutrel is not None:
+    write_mutrel(args.sampid, args.pairtree_params_fn, args.trees_fn, args.weight_trees_by, args.trees_mutrel)
 
 if __name__ == '__main__':
   main()
--- comparison/pastri/run.sh
+++ comparison/pastri/run.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-set -euo pipefail
-module load gnu-parallel
+#module load gnu-parallel
 
 BASEDIR=~/work/pairtree
-JOBDIR=$SCRATCH/jobs
+SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+JOBDIR=/tmp
 PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/sims.pairtree
-#BATCH=sims.pastri.informative
-#BATCH=sims.pastri.uniform
-BATCH=$1
+BATCH=sims.pastri.informative
 INDIR=$BASEDIR/scratch/inputs/$BATCH
 OUTDIR=$BASEDIR/scratch/results/$BATCH
 PARALLEL=80
 NUM_ITERS=10000
+PASTRI_DIR=$HOME/.apps/pastri
 
 function convert_inputs {
   mkdir -p $INDIR
 
   for ssmfn in $PAIRTREE_INPUTS_DIR/*.ssm; do
     sampid=$(basename $ssmfn | cut -d. -f1)
-    echo "python3 $BASEDIR/comparison/pastri/convert_inputs.py " \
+    echo "python3 $SCRIPTDIR/convert_inputs.py " \
       "--uniform-proposal" \
       "$PAIRTREE_INPUTS_DIR/$sampid.ssm" \
       "$PAIRTREE_INPUTS_DIR/$sampid.params.json" \
@@ -27,7 +26,7 @@ function convert_inputs {
   done | parallel -j$PARALLEL --halt 1
 }
 
-function run {
+function run_pastri {
   mkdir -p $OUTDIR
 
   for countsfn in $INDIR/*.counts; do
@@ -39,7 +38,7 @@ function run {
       # Miniconda.
       echo "source $HOME/.bash_host_specific && " \
         "cd $OUTDIR && " \
-        "python2 $HOME/.apps/pastri/src/RunPASTRI.py" \
+        "python2 $PASTRI_DIR/src/RunPASTRI.py" \
         "--output_prefix $runid" \
         "--num_iters $NUM_ITERS" \
         "$INDIR/${runid}.counts" \
@@ -50,9 +49,47 @@ function run {
   done | parallel -j$PARALLEL --joblog $SCRATCH/tmp/$BATCH.log
 }
 
+function get_F_and_C {
+  for treesfn in $OUTDIR/*.trees; do
+    runid=$(basename $treesfn | cut -d. -f1)
+    # Trees in $treesfn are ranked by likelihood. `get_F_and_C.py` takes tree
+    # rank as a parameter. Thus, if we count N trees with LH > 0, we know their
+    # ranks are [0, 1, ..., N-1].
+    valid_count=$(cat $treesfn | grep '^>' | grep -v -- '-inf$' | wc -l)
+    for idx in $(seq $valid_count); do
+      echo "source $HOME/.bash_host_specific && " \
+        "cd $OUTDIR && " \
+        "python2 $PASTRI_DIR/src/get_F_and_C.py" \
+        "-i $idx" \
+        "-o $OUTDIR/$runid" \
+        "$INDIR/${runid}.counts" \
+        "$treesfn" \
+        "$OUTDIR/${runid}.fsamples"
+    done
+  done | parallel -j$PARALLEL > /dev/null
+}
+
+function convert_outputs {
+  for tree_weights in llh uniform; do
+    for treesfn in $OUTDIR/*.trees; do
+      runid=$(basename $treesfn | cut -d. -f1)
+      echo "source $HOME/.bash_host_specific && " \
+        "cd $OUTDIR && " \
+        "OMP_NUM_THREADS=1 python3 $SCRIPTDIR/convert_outputs.py" \
+        "--weight-trees-by $tree_weights" \
+        "--trees-mutrel $OUTDIR/$runid.pastri_trees_$tree_weights.mutrel.npz" \
+        "$runid" \
+        "$PAIRTREE_INPUTS_DIR/$runid.params.json" \
+        "$treesfn"
+    done
+  done | parallel -j$PARALLEL > /dev/null
+}
+
 function main {
   #convert_inputs
-  run
+  #run_pastri
+  #get_F_and_C
+  convert_outputs
 }
 
 main
