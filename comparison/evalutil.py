8f03ae196b3d0393dbf4cd796e9c3f2a1a313cb4
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-01-19 12:00:29 -0500

    Create general method for computing mutrels from weighted tree samples.

--- comparison/evalutil.py
+++ comparison/evalutil.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import mutrel
 from common import Models
 
-def fix_rounding_errors(mrel):
+def _fix_rounding_errors(mrel):
   # Floating point error means that some entires can slightly exceed 1. Ensure
   # this doesn't happen by much.
   #
@@ -38,12 +38,96 @@ def add_garbage(posterior, garb_svids):
   mutrel.check_posterior_sanity(new_posterior.rels)
   return new_posterior
 
-def softmax(V):
-  V = np.copy(V) - np.max(V)
-  return np.exp(V) / np.sum(np.exp(V))
-
 def save_sorted_mutrel(mrel, mrelfn):
   mrel = mutrel.sort_mutrel_by_vids(mrel)
   mutrel.check_posterior_sanity(mrel.rels)
   np.savez_compressed(mrelfn, mutrel=mrel.rels)
 
+def _distinguish_unique(adjms, logweights, clusterings):
+  used_logweights = set()
+  uniq_adjms = []
+  uniq_clusterings = []
+  uniq_logweights = []
+  counts = []
+
+  for adjm, logweight, clustering in zip(adjms, logweights, clusterings):
+    clustering = tuple((set(C) for C in clustering))
+    for idx in range(len(uniq_adjms)):
+      # Note that having the same clustering and adjm as a previous sample
+      # doesn't imply that the LLH will be close -- PWGS may have assigned
+      # different phis between two such samples, leading to a different LLH.
+      # This wil not, however, be a problem for Pairtree, since it will always
+      # have the same inter-sample LLH in these cases.
+      #
+      # We use `used_logweights` as a cheap O(1) lookup for whether we've
+      # already seen a sampled `(adjm, logweight, clustering)` triplet,
+      # avoiding a quadratic scan through the lists when unnecessary. This
+      # relies on the logweights of equivalent samples being identical rather
+      # than just close, but that's okay -- we expect that two truly identical
+      # samples should have the same LLH, as LLH calculation should be
+      # deterministic.
+      if logweight in used_logweights and \
+      uniq_logweights[idx] == logweight and \
+      uniq_clusterings[idx] == clustering and \
+      np.array_equal(uniq_adjms[idx], adjm):
+        counts[idx] += 1
+        break
+    else:
+      uniq_adjms.append(adjm)
+      uniq_clusterings.append(clustering)
+      uniq_logweights.append(logweight)
+      used_logweights.add(logweight)
+      counts.append(1)
+
+  return (
+    uniq_adjms,
+    uniq_clusterings,
+    np.array(uniq_logweights),
+    np.array(counts),
+  )
+
+def _softmax(V):
+  V = np.copy(V) - np.max(V)
+  return np.exp(V) / np.sum(np.exp(V))
+
+def calc_mutrel_from_trees(adjms, llhs, clusterings, tree_weights):
+  if tree_weights == 'llh':
+    logweights = np.copy(llhs)
+  elif tree_weights == 'uniform':
+    logweights = np.zeros(len(llhs))
+  else:
+    raise Exception('Unknown tree_weights=%s' % tree_weights)
+
+  uniq_adjms, uniq_clusterings, uniq_logweights, counts = _distinguish_unique(adjms, logweights, clusterings)
+  # Oftentimes, we will have many samples of the same adjacency matrix paired
+  # with the same clustering. This will produce the same mutrel. As computing
+  # the mutrel from adjm + clustering is expensive, we want to avoid repeating
+  # this unnecessarily. Instead, we just modify the associated weight of the
+  # the pairing to reflect this.
+  #
+  # Weights are stored in log space. To get linear-space weights, we take the
+  # softmax of the logweights. Observe that if we have `C` copies of the
+  # logweight `W`, we obtain equivalent post-softmax linear-space weights under
+  # either of the following two methods:
+  #
+  # 1. (naive) Represent the associated samples `C` separate times in the softmax
+  # 2. (smart) Set `W' = W + log(C)`, as `exp(W') = Cexp(W)`
+  weights = _softmax(uniq_logweights + np.log(counts))
+
+  vids = None
+  idx = 0
+  for adjm, clustering, weight in zip(uniq_adjms, uniq_clusterings, weights):
+    idx += 1
+    mrel = mutrel.make_mutrel_tensor_from_cluster_adj(adjm, clustering)
+    if vids is None:
+      vids = mrel.vids
+      soft_mutrel = np.zeros(mrel.rels.shape)
+    else:
+      assert mrel.vids == vids
+    soft_mutrel += weight * mrel.rels
+
+  soft_mutrel = _fix_rounding_errors(soft_mutrel)
+  return mutrel.Mutrel(
+    vids = vids,
+    rels = soft_mutrel,
+  )
