c45e5d8c2fa9bbce0290b1e4cedc0625426fbe0e
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-01-27 17:20:25 -0500

    Use full mutrels object rather than just rels array.

--- comparison/eval.py
+++ comparison/eval.py
@@ -12,17 +12,17 @@ def load_mutrels(mutrel_args):
     mutrel_name, mutrel_path = mutrel_arg.split('=', 1)
     assert mutrel_name not in mutrels
     if os.path.exists(mutrel_path):
-      mutrels[mutrel_name] = np.load(mutrel_path)['mutrel']
-      assert np.all(0 <= mutrels[mutrel_name]) and np.all(mutrels[mutrel_name] <= 1)
+      mrel = np.load(mutrel_path)
+      mutrels[mutrel_name] = mutrel.Mutrel(vids=mrel['vids'], rels=mrel['rels'])
     else:
       mutrels[mutrel_name] = None
   return mutrels
 
 def compare(mutrels):
   assert 'truth' in mutrels
-  M, _, num_models = mutrels['truth'].shape
-  assert mutrels['truth'].shape == (M, M, num_models)
-  correct = np.argmax(mutrels['truth'], axis=2)
+  M, _, num_models = mutrels['truth'].rels.shape
+  assert mutrels['truth'].rels.shape == (M, M, num_models)
+  correct = np.argmax(mutrels['truth'].rels, axis=2)
 
   names = sorted(mutrels.keys())
   scores = {}
@@ -32,15 +32,19 @@ def compare(mutrels):
     if mrel is None:
       scores[name] = -1
       continue
-    assert mrel.shape == (M, M, num_models)
-    mutrel.check_posterior_sanity(mrel)
+
+    assert mrel.rels.shape == (M, M, num_models)
+    assert np.array_equal(mrel.vids, mutrels['truth'].vids)
+
+    mutrel.check_posterior_sanity(mrel.rels)
     score = np.nan * np.zeros((M, M))
     for I in range(M):
       for J in range(M):
         # I don't know how to use `correct` in this format to index into
         # `mutrels[name]`, hence the `I` and `J` for loops.
-        score[I,J] = mrel[I,J,correct[I,J]]
+        score[I,J] = mrel.rels[I,J,correct[I,J]]
     assert not np.any(np.isnan(score))
+    # Check truth as a sanity check.
     if name == 'truth':
       assert np.allclose(1, score)
     scores[name] = np.mean(score)
