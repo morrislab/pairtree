d2dea7c2095905e984450ebcb2396d04a7d7e119
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-02-21 13:51:18 -0500

    Convert params JSON to pickle to permit creating truth mutrels from params JSON.

---
+++ comparison/convert_adjlist_to_pickled_adjm.py
@@ -0,0 +1,31 @@
+import argparse
+import pickle
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import common
+import inputparser
+
+def main():
+  parser = argparse.ArgumentParser(
+    description='LOL HI THERE',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+  )
+  parser.add_argument('params_fn')
+  parser.add_argument('pickle_fn')
+  args = parser.parse_args()
+
+  params = inputparser.load_params(args.params_fn)
+  adjlist = inputparser.load_structure(params)
+  adjm = common.convert_adjlist_to_adjmatrix(adjlist)
+  with open(args.pickle_fn, 'wb') as outf:
+    pickle.dump({
+      'adjm': adjm,
+      'clusters': params['clusters'],
+      'vids_good': [V for C in params['clusters'] for V in C],
+      'vids_garbage': params['garbage'],
+    }, outf)
+
+if __name__ == '__main__':
+  main()
