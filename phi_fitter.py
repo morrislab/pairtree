e3e535c960e59b864f5a7e525517bb8e237bf607
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-05-21 10:51:45 -0400

    Only load modules when necessary.

--- phi_fitter.py
+++ phi_fitter.py
@@ -1,7 +1,5 @@
 import common
 import numpy as np
-import phi_fitter_iterative
-import phi_fitter_projection
 
 def fit_phis(adj, superclusters, supervars, method, iterations, parallel):
   key = (hash(adj.tobytes()), iterations)
@@ -17,13 +15,22 @@ fit_phis.cache_hits = 0
 fit_phis.cache_misses = 0
 
 def _fit_phis(adj, superclusters, supervars, method, iterations, parallel):
+  # Calling `import` on each function call should be cheap, as Python caches a
+  # reference to the module after the first load.
   if method in ('graddesc', 'rprop'):
+    import phi_fitter_iterative
     eta = phi_fitter_iterative.fit_etas(adj, superclusters, supervars, method, iterations, parallel)
+
   elif method == 'projection':
+    import phi_fitter_projection
     eta = phi_fitter_projection.fit_etas(adj, superclusters, supervars)
+
   elif method == 'proj_rprop':
+    import phi_fitter_projection
+    import phi_fitter_iterative
     eta_proj = phi_fitter_projection.fit_etas(adj, superclusters, supervars)
     eta = phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init=eta_proj)
+
   else:
     raise Exception('Unknown phi fitter %s' % method)
 
