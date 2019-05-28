5337fe719b2126e5c9358b8080dc3edc9a32e683
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-05-28 08:25:33 -0400

    Update run script.

--- comparison/pairtree/run.sh
+++ comparison/pairtree/run.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 set -euo pipefail
-#module load gnu-parallel
+command -v parallel > /dev/null || module load gnu-parallel
 
 PROTDIR=~/work/pairtree
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 BASEDIR=~/work/pairtree
 JOBDIR=~/jobs
 
-PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/sims.pairtree
-PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/sims.pairtree.fixedclusters
-#PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/steph.xeno.withgarb.pairtree
-#PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/steph.xeno.withgarb.pairtree
-
-PARALLEL=40
-TREE_CHAINS=40
+PARALLEL=1
+TREE_CHAINS=$PARALLEL
 BURNIN_PER_CHAIN=1000
 TREES_PER_CHAIN=2000
 PHI_ITERATIONS=10000
+PHI_FITTER=$1
+
+#BATCH=sims.pairtree.${PHI_FITTER}.singlechain
+BATCH=sims.pairtree
+PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/sims.pairtree
+PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/$BATCH
+#PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/steph.xeno.withgarb.pairtree
+#PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/steph.xeno.withgarb.pairtree
 
 function is_run_complete {
   if ([[ -f $1 ]] && unzip -l $1 | grep "adjm.npy" > /dev/null); then
@@ -28,11 +31,14 @@ function is_run_complete {
 
 function run_pairtree {
   mkdir -p $PAIRTREE_RESULTS_DIR
+  logfn=$BASEDIR/scratch/logs/${BATCH}.$(date '+%s').log
+  rm -f $logfn
 
   for ssmfn in $PAIRTREE_INPUTS_DIR/*.ssm; do
     runid=$(basename $ssmfn | cut -d. -f1)
     resultsfn="$PAIRTREE_RESULTS_DIR/$runid.results.npz"
     is_run_complete $resultsfn && continue
+    #[[ $runid =~ K30 || $runid =~ K100 ]] || continue
 
     jobfn=$(mktemp)
     (
@@ -44,9 +50,11 @@ function run_pairtree {
       echo "#SBATCH --output=$JOBDIR/slurm_${runid}_%j.txt"
       echo "#SBATCH --mail-type=NONE"
 
+        #"--only-build-tensor" \
       echo "cd $PAIRTREE_RESULTS_DIR && " \
-        "OMP_NUM_THREADS=1 " \
-        "PATH=$HOME/tmp/jose:$PATH " \
+        "TIMEFORMAT='%R %U %S'; time (OMP_NUM_THREADS=1 " \
+        "PATH=$HOME/tmp/jose/bin:$PATH " \
+        "LD_LIBRARY_PATH=$HOME/tmp/jose/bin:$LD_LIBRARY_PATH " \
         "python3 $PROTDIR/basic.py" \
         "--seed 1" \
         "--parallel $PARALLEL" \
@@ -54,16 +62,16 @@ function run_pairtree {
         "--trees-per-chain $TREES_PER_CHAIN" \
         "--burnin-per-chain $BURNIN_PER_CHAIN" \
         "--phi-iterations $PHI_ITERATIONS" \
-        "--phi-fitter projection" \
+        "--phi-fitter $PHI_FITTER" \
         "--params $PAIRTREE_INPUTS_DIR/${runid}.params.json" \
         "$PAIRTREE_INPUTS_DIR/$runid.ssm" \
         "$resultsfn" \
         ">$runid.stdout" \
-        "2>$runid.stderr"
-    ) > $jobfn
-    sbatch $jobfn
+        "2>$runid.stderr) 2>$runid.time"
+    ) #> $jobfn
+    #sbatch $jobfn
     rm $jobfn
-  done
+  done #| grep python3 | parallel -j40 --halt 1 --joblog $logfn
 }
 
 function convert_outputs {
@@ -90,8 +98,9 @@ function convert_outputs {
           ">${basepath}.output_conversion.stdout" \
           "2>${basepath}.output_conversion.stderr"
 
-        for use_subset in true false; do
-          for tree_weights in uniform llh; do
+        subset_size=2000
+        for use_subset in false; do
+          for tree_weights in llh; do
             basepath="${PAIRTREE_RESULTS_DIR}/${runid}.pairtree_trees."
             basepath+=$([[ "$use_subset" == "true" ]] && echo subset || echo all)
             basepath+=".${tree_weights}"
@@ -101,7 +110,7 @@ function convert_outputs {
             cmd+="--weight-trees-by $tree_weights "
             cmd+="--ssms ${PAIRTREE_INPUTS_DIR}/${runid}.ssm "
             if [[ "$use_subset" == "true" ]]; then
-              cmd+="--use-subset 2000 "
+              cmd+="--use-subset $subset_size "
             fi
             cmd+="$resultsfn "
             cmd+="${basepath}.mutphi.npz "
@@ -112,7 +121,7 @@ function convert_outputs {
             cmd="cd $PAIRTREE_RESULTS_DIR && "
             cmd+="OMP_NUM_THREADS=1 python3 $SCRIPTDIR/make_mutrels.py "
             if [[ "$use_subset" == "true" ]]; then
-              cmd+="--use-subset 2000 "
+              cmd+="--use-subset $subset_size "
             fi
             cmd+="--weight-trees-by $tree_weights "
             cmd+="--trees-mutrel ${basepath}.mutrel.npz "
