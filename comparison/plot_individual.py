358cbba4553bdc693bc61bf0779a4061feb10804
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-05-28 09:22:54 -0400

    Plot low S vs. high S.

--- comparison/plot_individual.py
+++ comparison/plot_individual.py
@@ -10,19 +10,21 @@ import os
 
 HAPPY_METHOD_NAMES = (
   ('truth', 'Truth'),
-  ('mle_unconstrained', 'MLE lineage frequencies'),
   ('pairtree_handbuilt', 'Pairtree (manually constructed trees)'),
-  ('pairtree_trees_llh', 'Pairtree'),
-  ('pastri_trees_llh', 'PASTRI'),
-  ('singlepairtree_trees_llh', 'Pairtree (single)'),
+  ('pairtree_proj_single_llh', 'Pairtree (single-chain)'),
+  ('pairtree_proj_multi_llh', 'Pairtree (multi-chain)'),
+  ('pastri_llh', 'PASTRI'),
+  ('lichee_llh', 'LICHeE'),
   ('pwgs_supervars_multi_llh', 'PhyloPlus'),
   ('pwgs_allvars_multi_llh', 'PhyloPlus (unclustered)'),
   ('pwgs_supervars_single_llh', 'PhyloWGS'),
   ('pwgs_allvars_single_llh', 'PhyloWGS (unclustered)'),
   ('pairtree_clustrel', 'Pairs tensor'),
+  ('mle_unconstrained', 'MLE lineage frequencies'),
 )
 METHOD_ORDER = {M: idx for idx, (M, M_full) in enumerate(HAPPY_METHOD_NAMES)}
 HAPPY_METHOD_NAMES = {M: M_full for (M, M_full) in HAPPY_METHOD_NAMES}
+MISSING = -1
 
 for name, synonym in (
 ):
@@ -56,13 +58,11 @@ def load_results(resultsfn):
       # Sometimes the LLH may be -inf. (Thanks, PASTRI.) Consider this to be a
       # failed run.
       print('%s has %s infs' % (M, np.sum(inf_idxs)), file=sys.stderr)
-      results.loc[inf_idxs,M] = -1
-
+      results.loc[inf_idxs,M] = MISSING
   return (results, methods)
 
 def get_method_names(results):
   methnames = [K for K in results.keys() if K != 'runid']
-  methnames = [M for M in methnames if not M.endswith('uniform')]
   return methnames
 
 def partition(results, methods, key):
@@ -80,13 +80,14 @@ def partition(results, methods, key):
 
     for M in methods:
       R = V_results[M]
-      failed = R == -1
+      failed = R == MISSING
       R_succ = R[np.logical_not(failed)]
       if len(R_succ) == 0:
         continue
       partitioned[V][M] = {
         'scores': R_succ,
-        'frac_complete': len(R_succ) / len(R),
+        'complete': len(R_succ),
+        'total': len(R),
       }
     if len(partitioned[V]) == 0:
       print('No method has output for %s=%s' % (key, V))
@@ -98,40 +99,64 @@ def sort_methods(methods):
   methods = sorted(methods, key = lambda M: METHOD_ORDER.get(M, 0))
   return methods
 
-def make_bar_traces(parted, _make_legend_label):
-  traces = []
-  for V in parted.keys():
-    methods = sort_methods(parted[V].keys())
-    trace = {
-      'type': 'bar',
-      'x': [HAPPY_METHOD_NAMES.get(M, M) for M in methods],
-      'y': [parted[V][M] for M in methods],
-    }
-    if V is None:
-      assert len(parted) == 1
-    else:
-      trace['name'] = _make_legend_label(V)
-    traces.append(trace)
-  return traces
+def make_bar_trace(methods, complete, total, name):
+  methods = sort_methods(methods)
+  complete = np.array([complete[M] for M in methods])
+  total = np.array([total[M] for M in methods])
+  frac_complete = 100*(complete / total)
 
-def make_box_traces(parted, _make_legend_label):
-  traces = []
-  for V in parted.keys():
-    methods = sort_methods(parted[V].keys())
-    points = [(HAPPY_METHOD_NAMES.get(M, M), R) for M in methods for R in parted[V][M]]
-    X, Y = zip(*points)
-    trace = {
-      'type': 'box',
-      'x': X,
-      'y': Y,
-      'boxmean': True
-    }
-    if V is None:
-      assert len(parted) == 1
-    else:
-      trace['name'] = _make_legend_label(V)
-    traces.append(trace)
-  return traces
+  trace = {
+    'type': 'bar',
+    'x': [HAPPY_METHOD_NAMES.get(M, M) for M in methods],
+    'y': frac_complete,
+    'name': name,
+  }
+  return trace
+
+def _make_points(vals):
+  methods = sort_methods(vals.keys())
+  points = [(HAPPY_METHOD_NAMES.get(M, M), R) for M in methods for R in vals[M]]
+  X, Y = zip(*points)
+  return (X, Y)
+
+def make_violin_trace(vals, side='both', group=None, name=None, bandwidth=None):
+  X, Y = _make_points(vals)
+  trace = {
+    'type': 'violin',
+    'x': X,
+    'y': Y,
+    'spanmode': 'hard',
+    'side': side,
+
+    #'box': {'visible': True},
+    'points': False,
+    #'pointpos': -1,
+    #'jitter': 0.5,
+    #'marker': {'size': 4, 'opacity': 0.7},
+    #'opacity': 0.7,
+    'bandwidth': bandwidth,
+    'width': 1.0,
+  }
+  if group is not None:
+    trace['legendgroup'] = group
+  if name is not None:
+    trace['name'] = name
+
+  return trace
+
+def make_box_trace(vals, group=None, name=None):
+  X, Y = _make_points(vals)
+  trace = {
+    'type': 'box',
+    'x': X,
+    'y': Y,
+  }
+  if group is not None:
+    trace['legendgroup'] = group
+  if name is not None:
+    trace['name'] = name
+
+  return trace
 
 def make_fig(traces, template, ytitle, max_y=None, layout_options=None):
   yaxis = {'title': ytitle}
@@ -148,6 +173,11 @@ def make_fig(traces, template, ytitle, max_y=None, layout_options=None):
     fig['layout'] = {**fig['layout'], **layout_options}
   return fig
 
+def read_plotly():
+  plotly_path = os.path.join(os.path.dirname(__file__), 'plotly', 'plotly.min.js')
+  with open(plotly_path) as F:
+    return F.read()
+
 def write_figs(figs, outfn):
   plot = ''
   for idx, fig in enumerate(figs):
@@ -160,19 +190,63 @@ def write_figs(figs, outfn):
     plot += plotly.offline.plot(
       fig,
       output_type = 'div',
-      include_plotlyjs = 'cdn',
+      include_plotlyjs = False,
       config = {
         'showLink': True,
         'toImageButtonOptions': {
-          'format': 'svg',
+          'format': 'png',
           'width': 750,
           'height': 450,
           'filename': imgfn,
         },
       },
     )
+
   with open(outfn, 'w') as outf:
+    #print('<script type="text/javascript">%s</script>' % read_plotly(), file=outf)
+    print( '<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>', file=outf)
     print(plot, file=outf)
+
+def set_missing_to(results, methods, val):
+  for M in methods:
+    missing = results[M] == MISSING
+    results.loc[missing,M] = val
+  return results
+
+def partition_by_threshold(parted, threshold):
+  def _combine(vals):
+    methods = set([M for V in vals for M in parted[V].keys()])
+    combined = {M: {
+      'scores': np.concatenate([parted[V][M]['scores'] if M in parted[V] else [] for V in vals]),
+      'complete': sum([parted[V][M]['complete'] if M in parted[V] else 0 for V in vals]),
+      'total': sum([parted[V][M]['total'] if M in parted[V] else 0 for V in vals]),
+    } for M in methods}
+    return combined
+
+  V_lte = [V for V in parted.keys() if V <= threshold]
+  V_gt  = [V for V in parted.keys() if V >  threshold]
+  threshed = {
+    'lte': _combine(V_lte),
+    'gt': _combine(V_gt),
+  }
+  return threshed
+
+def make_score_ytitle(score_type, plot_fn):
+  if score_type == 'mutrel':
+    if 'steph' in plot_fn:
+      ytitle = 'Tree reconstruction differences from expert baseline'
+    else:
+      ytitle = 'Tree reconstruction error'
+    ytitle += '<br>(% relations)'
+  elif score_type == 'mutphi':
+    if 'steph' in plot_fn:
+      ytitle = 'Frequency reconstruction differences from expert baseline'
+    else:
+      ytitle = 'Frequency reconstruction error'
+    ytitle += '<br>(Δbits / mutation / assay)'
+  else:
+    raise Exception('Unknown score type %s' % score_type)
+  return ytitle
 
 def main():
   parser = argparse.ArgumentParser(
@@ -180,81 +254,82 @@ def main():
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
   )
   parser.add_argument('--partition-by-samples', action='store_true')
+  parser.add_argument('--S-threshold', type=int)
   parser.add_argument('--template', default='seaborn')
   parser.add_argument('--max-y')
-  parser.add_argument('--plot-type', required=True, choices=('mutrel', 'mutphi'))
-  parser.add_argument('--hide-method', action='append')
+  parser.add_argument('--score-type', required=True, choices=('mutrel', 'mutphi'))
+  parser.add_argument('--baseline')
+  parser.add_argument('--bandwidth', type=float)
+  parser.add_argument('--plot-type', choices=('box', 'violin'), default='box')
   parser.add_argument('results_fn')
   parser.add_argument('plot_fn')
   args = parser.parse_args()
 
   results, methods = load_results(args.results_fn)
-  hidden_methods = set(args.hide_method) if args.hide_method is not None else set()
 
+  if args.score_type == 'mutrel':
+    set_missing_to(results, methods, 1)
+    for M in methods:
+      results[M] *= 100
+
+  if args.baseline is not None:
+    assert args.baseline in methods
+    for M in methods:
+      if M == args.baseline:
+        continue
+      present = results[M] != MISSING
+      results.loc[present,M] -= results.loc[present,args.baseline]
+
+  sides = {
+    'lte': 'negative',
+    'gt': 'positive',
+  }
+  names = {
+    'lte': 'Few tissue samples',
+    'gt': 'Many tissue samples',
+  }
+
+  score_traces = []
+  completion_traces = []
   if args.partition_by_samples:
     results = augment(results, 'S')
-    parted = partition(results, methods, key='S')
-  else:
-    parted = partition(results, methods, key=None)
+    parted_by_S = partition(results, methods, key='S')
+    parted_by_thresh = partition_by_threshold(parted_by_S, args.S_threshold)
+    for group in parted_by_thresh.keys():
+      vals = parted_by_thresh[group]
+      methods = [M for M in vals.keys() if M != 'truth']
+      scores = {M: vals[M]['scores'] for M in methods}
+      complete = {M: vals[M]['complete'] for M in methods}
+      total = {M: vals[M]['total'] for M in methods}
 
-  if args.plot_type == 'mutrel':
-    if 'steph' in args.plot_fn:
-      ytitle = 'Tree reconstruction differences from expert baseline'
-    else:
-      ytitle = 'Tree reconstruction error'
-    ytitle += '<br>(% relations)'
-  elif args.plot_type == 'mutphi':
-    if 'steph' in args.plot_fn:
-      ytitle = 'Frequency reconstruction differences from expert baseline'
-    else:
-      ytitle = 'Frequency reconstruction error'
-    ytitle += '<br>(Δbits / mutation / assay)'
-  else:
-    raise Exception('Unknown plot type %s' % args.plot_type)
+      if args.plot_type == 'box':
+        score_trace = make_box_trace(scores, group=group, name=names[group])
+      elif args.plot_type == 'violin':
+        score_trace = make_violin_trace(scores, side=sides[group], group=group, name=names[group], bandwidth=args.bandwidth)
+      else:
+        raise Exception('Unknown plot type %s' % args.plot_type)
 
-  figs = []
-  results_score = {}
-  results_frac_complete = {}
-  for V in parted:
-    results_score[V] = {M: parted[V][M]['scores'] for M in parted[V]}
-    results_frac_complete[V] = {M: parted[V][M]['frac_complete'] for M in parted[V]}
-  if args.plot_type == 'mutrel':
-    for V in parted:
-      for M in parted[V]:
-        results_score[V][M] *= 100
+      score_traces.append(score_trace)
+      completion_traces.append(make_bar_trace(methods, complete, total, name=group))
+  #else:
+  #  parted = partition(results, methods, key=None)
 
-  truth_method = None
-  for T in ('truth', 'pairtree_handbuilt'):
-    if T in methods:
-      truth_method = T
-      break
-  else:
-    raise Exception('truth_method %s not present' % truth_method)
-
-  if args.plot_type == 'mutphi':
-    for V in parted:
-      results_score[V] = {M: results_score[V][M] - results_score[V][truth_method] for M in results_score[V]}
-  for V in parted:
-    for M in (hidden_methods & set(results_score[V].keys())):
-      del results_score[V][M]
-      del results_frac_complete[V][M]
-
-  def _make_legend_label(V):
-    suffix = 'sample' if V == 1 else 'samples'
-    return '%s %s' % (V, suffix)
-  box_traces = make_box_traces(results_score, _make_legend_label)
-  bar_traces = make_bar_traces(results_frac_complete, _make_legend_label)
 
   figs = [
     make_fig(
-      box_traces,
+      score_traces,
       args.template,
-      ytitle,
+      make_score_ytitle(args.score_type, args.plot_fn),
       args.max_y,
-      {'boxmode': 'group'},
+      layout_options = {
+        'boxmode': 'group',
+        'violinmode': 'overlay',
+        #'violingap': 0.0,
+        #'violingroupgap': 0.0,
+      },
     ),
     make_fig(
-      bar_traces,
+      completion_traces,
       args.template,
       'Proportion of successful runs',
       max_y = None,
