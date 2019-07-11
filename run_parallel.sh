94ffba0f978faa805531a7e17881795dfa0d5be5
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-07-11 12:07:36 -0400

    Explicitly specify number of tasks to run in parallel.

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Usage: sbatch -N <num_nodes> run_parallel.sh jobs.txt jobs.log
-#
+
 #SBATCH --ntasks-per-node=40
 #SBATCH --time=23:58:00
 #SBATCH --job-name lolparallel
 #SBATCH --output /home/q/qmorris/jawinter/scratch/tmp/slurm-%j.stdout
 #SBATCH --error /home/q/qmorris/jawinter/scratch/tmp/slurm-%j.stderr
 
+PARALLEL=$SLURM_NTASKS_PER_NODE
+PARALLEL=10
 jobsfn=$1
 logfn=$2
 
@@ -15,4 +17,4 @@ module load gnu-parallel
 # DIRECTORY TO RUN - $SLURM_SUBMIT_DIR is the directory from which the job was submitted
 cd $SLURM_SUBMIT_DIR
 HOSTS=$(scontrol show hostnames $SLURM_NODELIST | tr '\n' ,)
-cat $jobsfn | parallel --halt 1 --resume --joblog $logfn -j $SLURM_NTASKS_PER_NODE -S $HOSTS --wd $PWD
+cat $jobsfn | parallel --halt 1 --resume --joblog $logfn -j $PARALLEL -S $HOSTS --wd $PWD
