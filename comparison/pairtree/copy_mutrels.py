462499b4b98750f7d978ea399812c95836d3b02b
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-08-19 18:19:50 -0400

    Improve runtime.

--- comparison/pairtree/copy_mutrels.py
+++ comparison/pairtree/copy_mutrels.py
@@ -10,17 +10,14 @@ def main():
   parser.add_argument('dest_results_fn')
   args = parser.parse_args()
 
-  with open(args.src_results_fn, 'rb') as F:
-    src_results = np.load(F)
-    # Must load into memory so the arrays persist after the file is closed.
-    src_results = {K: V for (K, V) in src_results.items()}
-
   desired = ['seed', 'clusters', 'garbage']
   for A in ('posterior', 'evidence'):
     for B in ('rels', 'vids'):
       desired.append('clustrel_%s_%s' % (A, B))
 
-  dest_results = {K: src_results[K] for K in desired}
+  with open(args.src_results_fn, 'rb') as F:
+    src_results = np.load(F)
+    dest_results = {K: src_results[K] for K in desired}
   np.savez_compressed(args.dest_results_fn, **dest_results)
 
 if __name__ == '__main__':
