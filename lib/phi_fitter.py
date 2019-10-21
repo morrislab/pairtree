ce348205cb1b26bdf6b0ccefee9603587f608ff8
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-10-21 13:29:38 -0400

    Implement new, more efficient rprop-based phi fitter that accounts for omega.

--- lib/phi_fitter.py
+++ lib/phi_fitter.py
@@ -2,6 +2,9 @@ import common
 import numpy as np
 
 def fit_phis(adj, superclusters, supervars, method, iterations, parallel):
+  if method == 'debug':
+    # Bypass cache when debugging.
+    return _fit_phis(adj, superclusters, supervars, method, iterations, parallel)
   key = (hash(adj.tobytes()), iterations)
   if key not in fit_phis.cache:
     fit_phis.cache[key] = _fit_phis(adj, superclusters, supervars, method, iterations, parallel)
@@ -13,6 +16,9 @@ def fit_phis(adj, superclusters, supervars, method, iterations, parallel):
 fit_phis.cache = {}
 fit_phis.cache_hits = 0
 fit_phis.cache_misses = 0
+
+# Used only for `rprop_cached`.
+last_eta = [None]
 
 def _fit_phis(adj, superclusters, supervars, method, iterations, parallel):
   # Calling `import` on each function call should be cheap, as Python caches a
@@ -30,6 +36,42 @@ def _fit_phis(adj, superclusters, supervars, method, iterations, parallel):
     import phi_fitter_iterative
     eta_proj = phi_fitter_projection.fit_etas(adj, superclusters, supervars)
     eta = phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init=eta_proj)
+
+  elif method == 'debug':
+    import phi_fitter_iterative
+    import phi_fitter_projection
+    import phi_fitter_lol
+    import tree_sampler
+    E = [
+      ('rprop_init_mle', phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init=None)),
+      #('rprop_init_dirichlet', phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init='dirichlet')),
+      #('rprop_init_lasteta', phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init=last_eta[0])),
+      ('lol_init_mle', phi_fitter_lol.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel)),
+      #('graddesc_init_mle', phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'graddesc', iterations, parallel, eta_init=None)),
+      #('graddesc_init_dirichlet', phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'graddesc', iterations, parallel, eta_init='dirichlet')),
+      #('graddesc_init_lasteta', phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'graddesc', iterations, parallel, eta_init=last_eta[0])),
+      #('graddesc_numerical', phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'graddesc_numerical', iterations, parallel, eta_init=None)),
+      ('projection', phi_fitter_projection.fit_etas(adj, superclusters, supervars)),
+    ]
+    #E.insert(3, ('rprop_init_proj', phi_fitter_iterative.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init=E[-1][1])))
+    eta = E[0][1]
+    last_eta[0] = np.copy(eta)
+
+    Z = common.make_ancestral_from_adj(adj)
+    svids = common.extract_vids(supervars)
+    ref_reads = np.array([supervars[svid]['ref_reads'] for svid in svids])
+    var_reads = np.array([supervars[svid]['var_reads'] for svid in svids])
+    omega = np.array([supervars[svid]['omega_v'] for svid in svids])
+    # `phi_fitter_iterative` assumes `omega` is 0.5, I think. I should fix this.
+    assert np.all(omega == 0.5)
+
+    K, S = eta.shape
+    nlglh = [(name, -tree_sampler._calc_llh_phi(np.dot(Z, E_prime), var_reads, ref_reads + var_reads, omega) / (K*S*np.log(2))) for name, E_prime in E]
+    names, scores = zip(*nlglh)
+    if True and not hasattr(_fit_phis, 'printed_header'):
+      print(*names, sep=',')
+      _fit_phis.printed_header = True
+    print(*scores, np.argsort(scores), sep=',', flush=True)
 
   else:
     raise Exception('Unknown phi fitter %s' % method)
---
+++ lib/phi_fitter_lol.py
@@ -0,0 +1,197 @@
+import numpy as np
+from numba import njit, jit, prange
+import common
+import binom
+
+# Matrices: M mutations, K clusters, S samples
+#   adj: KxK, adjacency matrix -- adj[a,b]=1 iff a is parent of b (with 1 on diagonal)
+#   Z: KxK, Z[a,b]=1 iff cluster a is ancestral to cluster b (with 1 on diagonal)
+#   A: MxK, A[m,k]=1 iff mut m is in cluster k
+#   phi: Kx1, per-cluster phis
+#   mut_phi: Mx1, per-mutation phis
+
+def fit_etas(adj, superclusters, supervars, method, iterations, parallel, eta_init=None):
+  # Supervar `i` is in cluster `i`. Cluster 0 is empty.
+  assert len(supervars) == len(adj) - 1
+  A = np.insert(np.eye(len(supervars)), 0, 0, axis=1)
+
+  svids = common.extract_vids(supervars)
+  arrs = {key: np.array([supervars[svid][key] for svid in svids]) \
+    for key in ('ref_reads', 'var_reads', 'omega_v')}
+  return _fit_etas(
+    adj,
+    A,
+    arrs['ref_reads'],
+    arrs['var_reads'],
+    arrs['omega_v'],
+    method,
+    iterations,
+    parallel,
+    eta_init,
+  )
+
+def _fit_etas(adj, A, ref_reads, var_reads, omega, method, iterations, parallel, eta_init=None):
+  M, K = A.shape
+  _, S = ref_reads.shape
+  assert ref_reads.shape == var_reads.shape == omega.shape == (M, S)
+
+  Z = common.make_ancestral_from_adj(adj)
+  # Numba only supports dot products on float arrays, not int arrays.
+  A = A.astype(np.float64)
+  Z = Z.astype(np.float64)
+
+  if eta_init is None:
+    phi_implied = (var_reads / (ref_reads + var_reads)) / omega
+    phi_implied = np.minimum(1, phi_implied)
+    phi_implied = np.maximum(0, phi_implied)
+    # Make first row 1 to account for normal root.
+    phi_implied = np.insert(phi_implied, 0, 1, axis=0)
+    # Since phi = Z.eta, we have eta = (Z^-1)phi.
+    eta = np.dot(np.linalg.inv(Z), phi_implied).T
+  elif str(eta_init) == 'dirichlet':
+    eta = np.random.dirichlet(alpha = K*[1e0], size = S)
+  else:
+    eta = np.copy(eta_init.T)
+  assert eta.shape == (S, K)
+
+  # This could be parallelized. Maybe using numba.prange?
+  for s in prange(S):
+    eta[s] = _fit_eta_S(
+      eta[s],
+      var_reads[:,s],
+      ref_reads[:,s],
+      omega[:,s],
+      A,
+      Z,
+      method,
+      iterations,
+    )
+
+  return eta.T
+
+@njit
+def _tile_rows(vec, repeats):
+  assert vec.ndim == 1
+  mat = np.empty((repeats, len(vec)))
+  mat[:] = vec
+  return mat
+
+@njit
+def _calc_grad(var_reads, ref_reads, omega, A, Z, psi):
+  M, K = A.shape
+  eta = softmax(psi)   # Kx1
+  phi = np.dot(Z, eta) # Kx1
+
+  phi_muts = np.dot(A, phi) # Mx1
+  dlogpx_dphi_muts = (var_reads / phi_muts) - (omega*ref_reads)/(1 - phi_muts*omega) # Mx1
+  dlogpx_dphi = np.dot(A.T, dlogpx_dphi_muts) # Kx1
+  assert dlogpx_dphi.shape == (K,)
+
+  psi_tiled = _tile_rows(psi, K)
+  assert psi_tiled.shape == (K, K)
+  # psi_sums[i,j] = psi[i] + psi[j]
+  psi_sums = psi_tiled + psi_tiled.T
+  logsumexp_psi = logsumexp(psi)
+  # deta_dpsi[i,j] = deta[i] / dpsi[j]
+  deta_dpsi = -np.exp(psi_sums - 2*logsumexp_psi) # KxK
+  np.fill_diagonal(deta_dpsi, (np.exp(psi + logsumexp_psi) - np.exp(2*psi)) / np.exp(2*logsumexp_psi))
+  assert deta_dpsi.shape == (K, K)
+
+  # beta[i,j] = sum of `deta_dpsi` values in column `j` that are relevant for phi
+  # `i` (i.e., where the entries in rows `a` where `a` is a descendant of node
+  # `i`)
+  beta = np.dot(Z, deta_dpsi)        # KxK
+  grad = np.dot(dlogpx_dphi.T, beta) # 1xK
+  assert grad.shape == (K,)
+  return grad
+
+@njit
+def _rprop(var_reads, ref_reads, omega, A, Z, psi, iterations, convergence_threshold):
+  # For explanation of `rprop`, see
+  # http://www.cs.toronto.edu/~tijmen/csc321/slides/lecture_slides_lec6.pdf.
+  step = 1e-4 * np.ones(psi.shape)
+  last_sign = np.ones(psi.shape)
+
+  for T in range(iterations):
+    grad = _calc_grad(var_reads, ref_reads, omega, A, Z, psi)
+    #grad_num = _calc_grad_numerical(var_reads, ref_reads, omega, A, Z, psi)
+    #print(grad, grad_num)
+    if np.max(np.abs(grad)) < convergence_threshold:
+      break
+    sign = np.ones(psi.shape)
+    sign[grad < 0] = -1
+    sign_agree = sign == last_sign
+    sign_disagree = np.logical_not(sign_agree)
+    last_sign = sign
+
+    delta = 1.2*sign_agree + 0.5*sign_disagree
+    delta = np.minimum(50, delta)
+    step *= delta
+    psi += sign * step
+
+  return psi
+
+@njit
+def logsumexp(V):
+  B = np.max(V)
+  log_sum = B + np.log(np.sum(np.exp(V - B)))
+  return log_sum
+
+@njit
+def softmax(V):
+  log_softmax = V - logsumexp(V)
+  smax = np.exp(log_softmax)
+  # The vector sum will be close to 1 at this point, but not close enough to
+  # make np.random.choice happy -- sometimes it will issue the error
+  # "probabilities do not sum to 1" from mtrand.RandomState.choice.
+  # To fix this, renormalize the vector.
+  smax /= np.sum(smax)
+  #assert np.isclose(np.sum(smax), 1)
+  return smax
+
+@njit
+def _fit_eta_S(eta_S, var_reads_S, ref_reads_S, omega_S, A, Z, method, iterations):
+  eta_S = np.maximum(common._EPSILON, eta_S)
+  psi_S = np.log(eta_S)
+
+  if method == 'rprop':
+    psi_S = _rprop(
+      var_reads_S,
+      ref_reads_S,
+      omega_S,
+      A,
+      Z,
+      psi_S,
+      iterations,
+      convergence_threshold=1e-5,
+    )
+  else:
+    raise Exception('Unknown psi fitter')
+
+  eta_S = softmax(psi_S)
+  return eta_S
+
+@njit
+def _calc_llh(var_reads, ref_reads, omega, A, Z, psi):
+  K = len(psi)
+  assert Z.shape == (K,K)
+  assert var_reads.shape == ref_reads.shape == omega.shape
+
+  eta = softmax(psi)
+  phi = np.dot(Z, eta) # Kx1
+  var_phis = np.dot(A, phi)
+  logp = binom.logpmf(var_reads, ref_reads + var_reads, var_phis * omega)
+  return np.sum(logp)
+
+@njit
+def _calc_grad_numerical(var_reads, ref_reads, omega, A, Z, psi):
+  _, K = A.shape
+  delta = 1e-10
+  grad = np.zeros(K)
+  for k in range(K):
+    psi_prime = np.copy(psi)
+    psi_prime[k] += delta
+    llh1 = _calc_llh(var_reads, ref_reads, omega, A, Z, psi)
+    llh2 = _calc_llh(var_reads, ref_reads, omega, A, Z, psi_prime)
+    grad[k] = (llh2 - llh1)/delta
+  return grad
