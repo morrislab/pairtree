374408f6564faff709695986597e874f52e7bad5
Jeff Wintersinger <jeff.git@wintersinger.com>
2019-11-07 22:41:20 -0500

    Refine debug output.

--- lib/phi_fitter.py
+++ lib/phi_fitter.py
@@ -18,7 +18,7 @@ fit_phis.cache_hits = 0
 fit_phis.cache_misses = 0
 
 # Used only for `rprop_cached`.
-last_eta = [None]
+last_eta = ['mle']
 
 def _calc_llh(phi, V, N, omega_v, epsilon=1e-5):
   import scipy
@@ -38,6 +38,9 @@ def _calc_llh(phi, V, N, omega_v, epsilon=1e-5):
   llh_per_sample = -np.sum(phi_llh, axis=0) / K
   nlglh = np.sum(llh_per_sample) / S
   return (phi_llh, llh_per_sample, nlglh)
+
+def lpdist(A, B, p=1):
+  return np.sum(np.abs(A - B)**p)**(1/p)
 
 def _fit_phis(adj, superclusters, supervars, method, iterations, parallel):
   # Calling `import` on each function call should be cheap, as Python caches a
@@ -67,7 +70,8 @@ def _fit_phis(adj, superclusters, supervars, method, iterations, parallel):
       #'lol_init_dirichlet': lambda: phi_fitter_lol.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init='dirichlet'),
       'projection': lambda: phi_fitter_projection.fit_etas(adj, superclusters, supervars),
     }
-    fitters['lol_init_proj'] = lambda: phi_fitter_lol.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init=fitters['projection']())
+    #fitters['lol_init_proj'] = lambda: phi_fitter_lol.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init=fitters['projection']())
+    #fitters['lol_init_prev'] = lambda: phi_fitter_lol.fit_etas(adj, superclusters, supervars, 'rprop', iterations, parallel, eta_init=last_eta[0])
 
     Z = common.make_ancestral_from_adj(adj)
     svids = common.extract_vids(supervars)
@@ -79,7 +83,8 @@ def _fit_phis(adj, superclusters, supervars, method, iterations, parallel):
     scores = {}
     times = {}
     zeros = {}
-    dists = {}
+    l1_dists = {}
+    l2_dists = {}
     for name, F in fitters.items():
       time_start = time.perf_counter_ns()
       etas[name] = F()
@@ -88,26 +93,28 @@ def _fit_phis(adj, superclusters, supervars, method, iterations, parallel):
       scores[name] = _calc_llh(phi, var_reads, total_reads, omega)
       times[name] = (time_end - time_start)/1e6
       zeros[name] = np.sum(phi == 0)
-      dists[name] = np.sum(np.abs(var_reads/(total_reads * omega) - phi[1:]))
+      l1_dists[name] = lpdist(var_reads/(total_reads * omega), phi[1:], p=1)
+      l2_dists[name] = lpdist(var_reads/(total_reads * omega), phi[1:], p=2)
 
     eta = etas['lol_init_mle']
     last_eta[0] = np.copy(eta)
 
     names = sorted(etas.keys())
+    sep = '\t'
     if True and not hasattr(_fit_phis, 'printed_header'):
-      print(*names, sep=',')
+      print(*names, sep=sep)
       _fit_phis.printed_header = True
-    if scores['lol_init_mle'][2] / scores['lol_init_proj'][2] < 0.9:
-      print('I WAS BORN, A UNICORN')
     print(
-      *[scores[name][2] for name in names],
+      *['%.3f' % scores[name][2] for name in names],
       np.nan,
-      *[times[name] for name in names],
+      *['%.3f' % times[name] for name in names],
       np.nan,
       *[zeros[name] for name in names],
       np.nan,
-      *[dists[name] for name in names],
-      sep=',',
+      *['%.3f' % l1_dists[name] for name in names],
+      np.nan,
+      *['%.3f' % l2_dists[name] for name in names],
+      sep=sep,
       flush=True
     )
 
