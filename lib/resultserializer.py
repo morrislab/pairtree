d746e067b3935edf5f219bf987eee76d8d4174a2
Jeff Wintersinger <jeff.git@wintersinger.com>
2020-02-24 05:12:44 -0500

    Switch to using zip rather than tar-backed archives.

--- lib/resultserializer.py
+++ lib/resultserializer.py
@@ -1,4 +1,4 @@
-import tarfile
+import zipfile
 import io
 import os
 import numpy as np
@@ -8,19 +8,30 @@ import time
 import mutrel
 
 class Results:
-  # Currently, this is backed by an LZMA-compressed tar archive, where each
+  # Currently, this is backed by an LZMA-compressed zip archive, where each
   # file within is either JSON-encoded or NumPy-encoded.
   # However, switching the implementation so it's backed by HDF5 instead should
   # be easy if I decide to do so -- the API as I've designed it here should
   # allow this.
+  #
+  # Initially, I used a tar rather than zip archive. However, tar is in an
+  # inferior format insofar as it does not allow seeking to specific files
+  # being read -- it alternates file headers with file names, so (potentially)
+  # the entire file must be decompressed to locate a specific file the user
+  # wants. Zip does not suffer this limitation.
+  #
+  # Pickle is a strictly unacceptable choice since it allows arbitrary code
+  # execution. If I used Pickle (as I initially did), users could not safely
+  # exchange results with one another.
 
   def __init__(self, fn):
     self._fn = fn
     self._to_add = {}
+    self._compress_type = zipfile.ZIP_LZMA
 
     if self._file_exists():
       with self._open() as F:
-        self._names = set([self._resolve_name(fullname) for fullname in F.getnames()])
+        self._names = set([self._resolve_name(fullname) for fullname in F.namelist()])
     else:
       self._names = set()
 
@@ -32,7 +43,7 @@ class Results:
 
   def _open(self, mode='r'):
     assert mode in ('r', 'w')
-    return tarfile.open(self._fn, '%s:xz' % mode, format=tarfile.PAX_FORMAT)
+    return zipfile.ZipFile(self._fn, mode, compression=self._compress_type,)
 
   def has(self, name):
     return name in self._names
@@ -40,23 +51,25 @@ class Results:
   def save(self):
     if self._file_exists():
       with self._open() as F:
-        for mem in F.getmembers():
-          fullname = mem.name
+        for zi in F.infolist():
+          fullname = zi.filename
           name = self._resolve_name(fullname)
           if name in self._to_add:
             continue
-          self._to_add[name] = {
-            'full_name': fullname,
-            'mtime': mem.mtime,
-            'bytes': F.extractfile(fullname).read(),
-          }
+          with F.open(zi) as G:
+            self._to_add[name] = {
+              'full_name': fullname,
+              'bytes': G.read(),
+              'timestamp': zi.date_time,
+            }
 
     with self._open('w') as F:
       for name, data in self._to_add.items():
-        ti = tarfile.TarInfo(data['full_name'])
-        ti.mtime = data['mtime']
-        ti.size = len(data['bytes'])
-        F.addfile(ti, io.BytesIO(data['bytes']))
+        zi = zipfile.ZipInfo(
+          filename=data['full_name'],
+          date_time=data['timestamp'],
+        )
+        F.writestr(zi, data['bytes'], compress_type=self._compress_type)
 
     self._to_add = {}
 
@@ -73,7 +86,7 @@ class Results:
 
     self._to_add[name] = {
       'full_name': '%s.%s' % (name, data_type),
-      'mtime': time.time(),
+      'timestamp': time.localtime(time.time()),
       'bytes': output,
     }
     self._names.add(name)
@@ -90,15 +103,14 @@ class Results:
     return self.has('%s_rels' % name) and self.has('%s_vids' % name)
 
   def _load(self, full_name, data_type, F):
-    reader = F.extractfile(full_name)
+    data = F.read(full_name)
     if data_type == 'npy':
-      # Hack. See https://github.com/numpy/numpy/issues/7989#issuecomment-340921579
-      # This hack likely means that two copies of the array will exist in memory temporarily. Bleh.
-      bio = io.BytesIO(reader.read())
-      bio.seek(0)
-      return np.load(bio)
+      bio = io.BytesIO(data)
+      # `allow_pickle` was only made False by default in Numpy 1.16.3, so force
+      # it here for older Numpy versions.
+      return np.load(bio, allow_pickle=False)
     elif data_type == 'json':
-      return json.load(reader)
+      return json.loads(data.decode('utf-8'))
     else:
       raise Exception('Unknown data type: %s' % data_type)
 
@@ -109,7 +121,7 @@ class Results:
     results = {}
 
     with self._open() as F:
-      present = set(F.getnames())
+      present = set(F.namelist())
       for name in names:
         for data_type in ('npy', 'json'):
           full_name = '%s.%s' % (name, data_type)
