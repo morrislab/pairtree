08c985717acbb94335d05e22ceb95df075623f50
Jeff Wintersinger <jeff.git@wintersinger.com>
2020-03-06 16:34:30 -0500

    Improve Pairtree run script.

--- comparison/pairtree/run.sh
+++ comparison/pairtree/run.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -euo pipefail
-command -v parallel > /dev/null || module load gnu-parallel
+#command -v parallel > /dev/null || module load gnu-parallel
 
 BASEDIR=~/work/pairtree
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 JOBDIR=~/jobs
+PYTHON3=$HOME/.apps/miniconda3/bin/python3
 
 PARALLEL=40
 TREE_CHAINS=$PARALLEL
-TREES_PER_CHAIN=3000
+TREES_PER_CHAIN=30000
 PHI_ITERATIONS=10000
-PHI_FITTER=rprop
+PHI_FITTER=projection
 THINNED_FRAC=1.0
 BURNIN=0.333333
 
 BATCH=sims.smallalpha.pairtree
 PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/$BATCH
 TRUTH_DIR=$BASEDIR/scratch/results/sims.smallalpha.truth
-PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/${BATCH}.rprop
+PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/${BATCH}.${PHI_FITTER}
 
 #BATCH=steph.pairtree.multichain
 #PAIRTREE_INPUTS_DIR=$BASEDIR/scratch/inputs/steph.xeno.withgarb.pairtree
@@ -26,51 +27,51 @@ PAIRTREE_RESULTS_DIR=$BASEDIR/scratch/results/${BATCH}.rprop
 source $SCRIPTDIR/util.sh
 
 function run_pairtree {
-  logfn=$BASEDIR/scratch/logs/${BATCH}.$(date '+%s').log
-  rm -f $logfn
-
   for ssmfn in $PAIRTREE_INPUTS_DIR/*.ssm; do
     runid=$(basename $ssmfn | cut -d. -f1)
     outdir="$PAIRTREE_RESULTS_DIR/$runid"
     resultsfn="$outdir/$runid.results.npz"
-    [[  $runid =~ K100_ ]] && continue
+    [[  $runid =~ K100_ ]] || continue
     #is_big_K $runid && continue
     is_run_complete $resultsfn && continue
     #[[ -f $resultsfn ]] || continue
 
     jobfn=$(mktemp)
-    (
-      echo "#!/bin/bash"
-      echo "#SBATCH --nodes=1"
-      echo "#SBATCH --ntasks=$PARALLEL"
-      echo "#SBATCH --time=23:59:00"
-      echo "#SBATCH --job-name $runid"
-      echo "#SBATCH --output=$JOBDIR/slurm_${runid}_%j.txt"
-      echo "#SBATCH --mail-type=NONE"
+    cmd=""
+    cmd+="#!/bin/bash\n"
+    cmd+="#SBATCH --nodes=1\n"
+    cmd+="#SBATCH --ntasks=$PARALLEL\n"
+    cmd+="#SBATCH --mem=20GB\n"
+    cmd+="#SBATCH --time=95:59:00\n"
+    cmd+="#SBATCH --job-name pairtree_$runid\n"
+    cmd+="#SBATCH --output=$JOBDIR/slurm_pairtree_${runid}_%j.txt\n"
+    cmd+="#SBATCH --partition=cpu\n"
+    cmd+="#SBATCH --mail-type=NONE\n"
 
-        #"--only-build-tensor" \
-        #"--verbose" \
-      echo "mkdir -p $outdir && cd $outdir && " \
-        "TIMEFORMAT='%R %U %S'; time (" \
-        "OMP_NUM_THREADS=1 " \
-        "python3 $BASEDIR/bin/pairtree" \
-        "--seed 1" \
-        "--parallel $PARALLEL" \
-        "--tree-chains $TREE_CHAINS" \
-        "--trees-per-chain $TREES_PER_CHAIN" \
-        "--phi-iterations $PHI_ITERATIONS" \
-        "--phi-fitter $PHI_FITTER" \
-        "--params $PAIRTREE_INPUTS_DIR/${runid}.params.json" \
-        "--thinned-frac $THINNED_FRAC" \
-        "--burnin $BURNIN" \
-        "--gamma 0.7" \
-        "--zeta 0.7" \
-        "--iota 0.7" \
-        "$PAIRTREE_INPUTS_DIR/$runid.ssm" \
-        "$resultsfn" \
-        ">$runid.stdout" \
-        "2>$runid.stderr) 2>$runid.time"
-    ) > $jobfn
+    cmd+="mkdir -p $outdir && cd $outdir && "
+    cmd+="TIMEFORMAT='%R %U %S'; time ( "
+    cmd+="OMP_NUM_THREADS=1  "
+    cmd+="$PYTHON3 $BASEDIR/bin/pairtree "
+    cmd+="--seed 1 "
+    cmd+="--parallel $PARALLEL "
+    cmd+="--tree-chains $TREE_CHAINS "
+    cmd+="--trees-per-chain $TREES_PER_CHAIN "
+    cmd+="--phi-iterations $PHI_ITERATIONS "
+    cmd+="--phi-fitter $PHI_FITTER "
+    cmd+="--params $PAIRTREE_INPUTS_DIR/${runid}.params.json "
+    cmd+="--thinned-frac $THINNED_FRAC "
+    cmd+="--burnin $BURNIN "
+    cmd+="--gamma 0.7 "
+    cmd+="--zeta 0.7 "
+    cmd+="--iota 0.7 "
+    #cmd+="--only-build-tensor "
+    #cmd+="--verbose "
+    cmd+="$PAIRTREE_INPUTS_DIR/$runid.ssm "
+    cmd+="$resultsfn "
+    cmd+=">$runid.stdout "
+    cmd+="2>$runid.stderr) 2>$runid.time "
+
+    echo -e $cmd > $jobfn
     sbatch $jobfn
     rm $jobfn
   done
@@ -86,7 +87,7 @@ function create_neutree {
 
     (
       cmd="cd $outdir && "
-      cmd+="OMP_NUM_THREADS=1 python3 $SCRIPTDIR/convert_outputs.py "
+      cmd+="OMP_NUM_THREADS=1 $PYTHON3 $SCRIPTDIR/convert_outputs.py "
       cmd+="$resultsfn "
       cmd+="${PAIRTREE_INPUTS_DIR}/${runid}.params.json "
       cmd+="${basepath}.neutree.pickle "
@@ -105,7 +106,7 @@ function create_mutrel_from_clustrel {
 
     (
       cmd="cd $outdir && "
-      cmd+="OMP_NUM_THREADS=1 python3 $SCRIPTDIR/make_mutrel_from_clustrel.py "
+      cmd+="OMP_NUM_THREADS=1 $PYTHON3 $SCRIPTDIR/make_mutrel_from_clustrel.py "
       cmd+="$resultsfn "
       cmd+="${basepath}.clustrel_mutrel.npz "
       echo $cmd
@@ -114,9 +115,9 @@ function create_mutrel_from_clustrel {
 }
 
 function main {
-  #run_pairtree #| grep python3 | parallel -j80 --halt 1 --eta
-  create_neutree | parallel -j80 --halt 1 --eta
-  create_mutrel_from_clustrel | sort --random-sort | parallel -j5 --halt 1 --eta
+  run_pairtree #| grep python3 | parallel -j80 --halt 1 --eta
+  #create_neutree | parallel -j80 --halt 1 --eta
+  #create_mutrel_from_clustrel | sort --random-sort | parallel -j5 --halt 1 --eta
 }
 
 main
