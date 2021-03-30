a71aba3bc5e749351fc02f4098a0ed8e41433e5d
Jeff Wintersinger <jeff.git@wintersinger.com>
2021-03-30 14:10:28 -0400

    Make summposterior respect hidden_samples in params.json.

--- lib/plotutil.py
+++ lib/plotutil.py
@@ -12,3 +12,12 @@ def read_file(fn, basedir=None):
 
 def js_on_load(js):
   return '<script type="text/javascript">document.addEventListener("DOMContentLoaded", () => { %s });</script>' % js
+
+def hide_samples(sampnames, hidden_samples):
+  if hidden_samples is None:
+    return None
+  hidden = set(hidden_samples)
+  sampset = set(sampnames)
+  assert hidden.issubset(sampset) and len(hidden) < len(sampset)
+  visible_sampidxs = [idx for idx, samp in enumerate(sampnames) if samp not in hidden]
+  return visible_sampidxs
