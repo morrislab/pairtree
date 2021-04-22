ed6d0bc1000de350eb0026a174d3245b104f19ab
Jeff Wintersinger <jeff.git@wintersinger.com>
2021-04-22 17:39:08 -0400

    Implement test based on Bayes factors.

--- util/remove_bad_alpha.py
+++ util/remove_bad_alpha.py
@@ -4,6 +4,7 @@
 
 import argparse
 import numpy as np
+import scipy.special
 import scipy.stats
 import json
 
@@ -12,47 +13,68 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 import common
 import inputparser
+import util
 
-def _remove_bad(ssms, phi_hat_threshold, quantile, print_bad=False):
-  # Note that SSMs are modified in-place.
-  alpha0 = 1
-  beta0 = 1
-  # To flag only extreme cases, set this above 1.0 -- e.g., to 1.5.
-  phi_mle_threshold = 1.0
+def _calc_logbf(ssm, omega1=1):
+  V = ssm['var_reads']
+  R = ssm['ref_reads']
+  S = len(V)
 
+  omega = [np.broadcast_to(omega1, S), ssm['omega_v']]
+  logp = []
+
+  for om in omega:
+    integ = scipy.special.betainc(V+1, R+1, om)
+    # Prevent division by zero.
+    loginteg = np.log(np.maximum(1e-30, integ))
+    logp.append(-np.log(om) + loginteg)
+
+  logbf = logp[0] - logp[1]
+  # Convert from nats to bits.
+  logbf /= np.log(2)
+
+  return logbf
+
+def _remove_bad(ssms, logbf_thresh, print_bad=False):
   bad = []
   bad_count = 0
   total_count = 0
 
+  alpha0 = 1
+  beta0 = 1
+  phi_hat_threshold = 1 - 1e-2
+
   for vid, V in ssms.items():
+    logbfs = _calc_logbf(V)
+    is_samp_bad = logbfs > logbf_thresh
+
+    bad_count   += np.sum(is_samp_bad)
+    total_count += len(is_samp_bad)
+
+    # Following is just used for diagnostics and is not part of decision for bad vs. good.
+    phi_mle = V['var_reads'] / (V['omega_v'] * V['total_reads'])
     phi_alpha = alpha0 + V['var_reads']
     phi_beta = beta0 + np.maximum(0, V['omega_v']*V['total_reads'] - V['var_reads'])
-    # Binary decision: is the mass in the `phi ~ Beta(...)` CDF below
-    # the `quantile` threshold?
-    probs = scipy.stats.beta.cdf(phi_hat_threshold, phi_alpha, phi_beta)
+    # How much mass in our phi_hat posterior is in the region [phi_hat_threshold, 1]?
+    probs = 1. - scipy.stats.beta.cdf(phi_hat_threshold, phi_alpha, phi_beta)
 
-    phi_mle = V['var_reads'] / (V['omega_v'] * V['total_reads'])
-    bad_samps = np.logical_and.reduce((
-      probs < quantile,
-      phi_mle > phi_mle_threshold,
-    ))
-
-    if np.any(bad_samps):
+    if np.any(is_samp_bad):
       bad.append(vid)
-    if print_bad and np.any(bad_samps):
+    if print_bad and np.any(is_samp_bad):
       print(vid, np.vstack((
         V['var_reads'],
         V['total_reads'],
         phi_mle,
         probs,
-        bad_samps,
-      )), sep='\n')
-      print('')
-
-    bad_count += np.sum(bad_samps)
-    total_count += len(bad_samps)
+        is_samp_bad,
+        logbfs,
+      )),
+      '',
+      sep='\n'
+    )
 
   bad_samp_prop = bad_count / total_count
+  assert 0 <= bad_samp_prop <= 1
   return (bad, bad_samp_prop)
 
 def main():
@@ -60,11 +82,10 @@ def main():
     description='LOL HI THERE',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
   )
-  parser.add_argument('--phi-hat-threshold', type=float, default = 1 - 1e-2,
+  parser.add_argument('--logbf-threshold', type=float, default=10.,
     help='Blah')
-  parser.add_argument('--quantile', type=float, default = 0.5,
-    help='Blah')
-  parser.add_argument('--print-bad-data', action='store_true')
+  parser.add_argument('--print-bad', action='store_true')
+  parser.add_argument('--keep-existing-garbage', action='store_true')
   parser.add_argument('in_ssm_fn')
   parser.add_argument('in_params_fn')
   parser.add_argument('out_params_fn')
@@ -75,9 +96,12 @@ def main():
 
   ssms = inputparser.load_ssms(args.in_ssm_fn)
   params = inputparser.load_params(args.in_params_fn)
-  ssms = inputparser.remove_garbage(ssms, params['garbage'])
+  if args.keep_existing_garbage:
+    params['garbage'] = []
+  else:
+    ssms = inputparser.remove_garbage(ssms, params['garbage'])
 
-  bad_vids, bad_samp_prop = _remove_bad(ssms, args.phi_hat_threshold, args.quantile, args.print_bad_data)
+  bad_vids, bad_samp_prop = _remove_bad(ssms, args.logbf_threshold, args.print_bad)
   bad_ssm_prop = len(bad_vids) / len(ssms)
   if len(bad_vids) > 0:
     params['garbage'] = common.sort_vids(params['garbage'] + bad_vids)
