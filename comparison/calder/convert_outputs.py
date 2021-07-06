c64ead4be75cc6227bd024ffd113b10be94ac297
Jeff Wintersinger <jeff.git@wintersinger.com>
2021-07-06 16:18:27 -0400

    Implement output conversion.

--- comparison/calder/convert_outputs.py
+++ comparison/calder/convert_outputs.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import re
 
 import sys
 import os
@@ -7,6 +8,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'lib'))
 import neutree
 import inputparser
+import common
 
 def _load_mat(F):
   name = None
@@ -18,39 +20,110 @@ def _load_mat(F):
       break
 
   col_labels = next(F).strip().split(',')
+  row_labels = []
   while True:
-    fields = next(F).strip().split(',')
-    row_label = fields[0]
+    try:
+      line = next(F).strip()
+    except StopIteration:
+      break
+    if len(line) == 0:
+      break
+    fields = line.split(',')
+    row_labels.append(fields[0])
     row_vals = np.array([float(V) for V in fields[1:]])
     mat.append(row_vals)
-  mat = np.array(mat)
 
-  return (name, mat)
+  assert name is not None and len(col_labels) > 0 and len(row_labels) > 0
+  mat = np.array(mat)
+  # Return transpose.
+  return (name, col_labels, row_labels, mat.T)
 
 def _load_mats(matfn):
   mats = {}
+  row_labels = {}
+  col_labels = {}
+
   with open(matfn) as F:
     while True:
-      name, mat = _load_mat(F)
+      try:
+        name, row_lab, col_lab, mat  = _load_mat(F)
+      except StopIteration:
+        break
       mats[name] = mat
-  return mats
+      row_labels[name] = row_lab
+      col_labels[name] = col_lab
+  return (mats, row_labels, col_labels)
 
-def _load_struct(structfn):
-  pass
+def _load_struct(svids, structfn):
+  # NB: Calder seems to always assume monoprimary tree, so it doesn't represent
+  # normal root in tree structure (implying you can't have polyprimary).
+  patterns = {
+    'label':  r'^(v\d+) \[label="([^"]+)"\];$',
+    'edge':   r'^([a-z0-9]+) -> ([a-z0-9]+);$',
+    'eof':    r'^}$',
+    'header': r'^digraph sim_tree1 {$',
+  }
+  node_map = {}
+  edges = []
 
+  with open(structfn) as F:
+    while True:
+      line = next(F).strip()
+      for line_type, pat in patterns.items():
+        R = re.search(pat, line)
+        if R:
+          break
+      else:
+        raise Exception('No match for line: %s' % line)
 
-def convert(params_fn, trees_fn, neutree_fn):
-  N = len(structs)
+      if line_type == 'header':
+        continue
+      if line_type == 'label':
+        node_map[R.group(1)] = R.group(2)
+      elif line_type == 'edge':
+        edges.append((R.group(1), R.group(2)))
+      elif line_type == 'eof':
+        break
+      else:
+        raise Exception('Unknown line_type %s' % line_type)
+
+  parents = {node_map[B]: node_map[A] for (A,B) in edges}
+  struct = np.array([svids.index(parents[child]) for child in parents.keys()])
+  assert len(struct) == len(svids) - 1
+  return struct
+
+def convert(params_fn, calder_mats_fn, calder_trees_fn, neutree_fn):
   params = inputparser.load_params(params_fn)
+
+  mats, row_labels, col_labels = _load_mats(calder_mats_fn)
+  assert row_labels['Fhat'][0] == 'samples'
+  svids = row_labels['Fhat'][1:]
+  assert svids == common.sort_vids(svids)
+
+  struct = _load_struct(svids, calder_trees_fn)
   ntree = neutree.Neutree(
-    structs = structs,
-    phis = phis,
-    counts = np.ones(N),
-    logscores = llhs,
-    clusterings = clusterings,
+    structs = [struct],
+    phis = [mats['Fhat']],
+    counts = np.array([1]),
+    logscores = np.array([0.]),
+    clusterings = [params['clusters']],
     garbage = params['garbage'],
   )
   neutree.save(ntree, neutree_fn)
+
+def _is_full_tree(stdout_fn):
+  # No way to tell without checking CALDER STDOUT whether it built a tree
+  # incorporating all mutations.
+  with open(stdout_fn) as F:
+    S = F.read()
+  M = re.search(r'Maximal tree contains (\d+) out of (\d+) mutations/clusters\.', S)
+  if not M:
+    return False
+  A, B = M.groups()
+  if A != B:
+    assert int(A) < int(B)
+    return False
+  return True
 
 def main():
   parser = argparse.ArgumentParser(
@@ -60,10 +133,13 @@ def main():
   parser.add_argument('pairtree_params_fn')
   parser.add_argument('calder_mats_fn')
   parser.add_argument('calder_trees_fn')
+  parser.add_argument('calder_stdout_fn')
   parser.add_argument('neutree_fn')
   args = parser.parse_args()
 
-  convert(args.sampid, args.pairtree_params_fn, args.trees_fn, args.neutree_fn)
+  #if not _is_full_tree(args.calder_stdout_fn):
+  #  raise Exception('not full tree')
+  convert(args.pairtree_params_fn, args.calder_mats_fn, args.calder_trees_fn, args.neutree_fn)
 
 if __name__ == '__main__':
   main()
